0d02188efac24d58e1bd96b06f8702d85fb16c8a
Kyle Birnbaum <kylebirnbaum@gmail.com>
2022-03-20 15:15:50 -0700

    changed main.py pip installer so it wont fail when something is printed to stdout like a warning

--- main.py
+++ main.py
@@ -7,11 +7,4 @@ if __name__ == "__main__":
 	dir_path = os.path.dirname(os.path.realpath(__file__))
 	os.chdir(dir_path)
 	pipe = subprocess.Popen("pip install -r requirements.txt", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	out, err = pipe.communicate()
-	error = err.decode()
-	print(error)
-	if not error:
-		eote.main()
-	else:
-		print("Could Not Install Requirements.")
-		print(error)
+	eote.main()
